a408da0efe8ae2643fd9861b5d08c3c70ffe1fac
sayan jana <jana.sayan7@gmail.com>
2025-08-31 09:11:39 +0200

    update

--- run-platform-tests-and-allure.sh
+++ run-platform-tests-and-allure.sh
@@ -4,9 +4,9 @@
 # Start test-platform server
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 ls -la
-cd "$SCRIPT_DIR/test-platform"
+cd "$SCRIPT_DIR"
 ls -la
-cd "$SCRIPT_DIR/test-platform/submodules/sample-app" || exit 1
+cd "$SCRIPT_DIR/sample-app" || exit 1
 npm i && npm start &
 PLATFORM_PID=$!
 
--- run-runner.sh
+++ run-runner.sh
@@ -3,7 +3,7 @@
 
 # Start test-platform server
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPT_DIR/test-platform/submodules/sample-app" || exit 1
+cd "$SCRIPT_DIR/sample-app" || exit 1
 npm i && npm start &
 PLATFORM_PID=$!
 
